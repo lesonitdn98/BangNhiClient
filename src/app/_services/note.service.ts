a35ed161dc92990fbcb7628041ff81ecce863343
lesonnnn <48326957+lesonitdn98@users.noreply.github.com>
2021-05-16 23:31:39 +0700

    huhuh

--- src/app/_services/note.service.ts
+++ src/app/_services/note.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { map } from 'rxjs/operators';
+import { Note } from '@app/_models';
 
 @Injectable({ providedIn: 'root' })
 export class NoteService {
@@ -10,6 +11,24 @@ export class NoteService {
 
     getAll() {
         return this.http.get<any>(`${environment.apiUrl}/notes`)
+            .pipe(map(response => {
+                if (response.ok) {
+                    return response.data;
+                }
+            }));
+    }
+
+    newNote(title: string, description: string) {
+        return this.http.post<any>(`${environment.apiUrl}/notes/add?title=${title}&description=${description}`, null)
+            .pipe(map(response => {
+                if (response.ok) {
+                    return response.data;
+                }
+            }));
+    }
+
+    removeNote(note: Note) {
+        return this.http.post<any>(`${environment.apiUrl}/notes/remove?noteId=${note.id}`, null)
             .pipe(map(response => {
                 if (response.ok) {
                     return response.data;
--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -2,26 +2,63 @@ import { Component } from '@angular/core';
 import { first } from 'rxjs/operators';
 
 import { Note } from '@app/_models';
+import { User } from '@app/_models';
+import { UserService } from '@app/_services';
 import { NoteService } from '@app/_services';
+import { AuthenticationService } from '../_services';
 
 @Component({ templateUrl: 'note.component.html' })
 export class NoteComponent {
     loading = false;
     notes!: Note[];
+    user!: User;
+    users!: User[];
     currentNote!: Note;
 
-    constructor(private noteService: NoteService) { }
+    constructor(
+        private noteService: NoteService,
+        private userService: UserService,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.user.subscribe((x: User) => this.user = x);
+    }
 
     ngOnInit() {
         this.loading = true;
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.loading = false;
+            this.users = users;
+        });
+        this.getAllNotes();
+    }
+
+    getAllNotes() {
         this.noteService.getAll().pipe(first()).subscribe(notes => {
             this.loading = false;
-            this.notes = notes;
-            this.currentNote = notes[0];
+            this.notes = notes.slice().reverse();
+            this.currentNote = this.notes[0];
         });
     }
 
     setCurrentNote(note: Note) {
         this.currentNote = note
+    }
+
+    addNewNote() {
+        if (!this.loading) {
+            this.loading = true;
+            this.noteService.newNote("New Note", "").pipe(first()).subscribe(data => {
+                this.getAllNotes();
+            })
+        }
+    }
+
+    removeNote() {
+        if (!this.loading) {
+            this.loading = true;
+            this.noteService.removeNote(this.currentNote).pipe(first()).subscribe(data => {
+                this.getAllNotes();
+            })
+        }
     }
 }
